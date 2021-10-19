1956bad1e1b8f4a43e3a849b081521223917b14e
Antonio Sousa <antoniowsj@outlook.com>
2021-10-19 14:29:39 -0300

    Shoot function

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ function start() { // Inicio da função start()
 
   //Principais variáveis do jogo
 
+  var podeAtirar = true;
   var jogo = {}
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -81,7 +82,8 @@ function start() { // Inicio da função start()
 
     if (jogo.pressionou[TECLA.A]) {
 
-      //Chama função Disparo	
+      //Chama função Disparo
+      disparo();
     }
 
   } // fim da função movejogador()
@@ -114,7 +116,7 @@ function start() { // Inicio da função start()
   function moveamigo() {
 
     posicaoX = parseInt($("#amigo").css("left"));
-    $("#amigo").css("left", posicaoX + 1);
+    $("#amigo").css("left", posicaoX + 3);
 
     if (posicaoX > 906) {
 
@@ -123,6 +125,39 @@ function start() { // Inicio da função start()
     }
 
   } // fim da função moveamigo()
+
+  function disparo() {
+
+    if (podeAtirar == true) {
+
+      podeAtirar = false;
+
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX = parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro = topo + 37;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top", topoTiro);
+      $("#disparo").css("left", tiroX);
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30);
+
+    } //Fecha podeAtirar
+
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left", posicaoX + 19);
+
+      if (posicaoX > 900) {
+
+        window.clearInterval(tempoDisparo);
+        tempoDisparo = null;
+        $("#disparo").remove();
+        podeAtirar = true;
+
+      }
+    } // Fecha executaDisparo()
+  } // Fecha disparo()
 
 
 
